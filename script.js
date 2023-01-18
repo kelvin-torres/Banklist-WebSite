9bca39b0bc401be1801681e75f3e04e58efb31f2
kelvin-torres <kelvinlgtorres@gmail.com>
2023-01-18 10:03:22 -0500

    Implementing Smooth Scrolling

--- script.js
+++ script.js
@@ -34,10 +34,100 @@ document.addEventListener('keydown', function (e) {
 ////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////
 
-console.log(document.documentElement);
-console.log(document.head);
-console.log(document.body);
+//Selecting elements
+// console.log(document.documentElement);
+// console.log(document.head);
+//console.log(document.body);
 
-document.querySelector('.header');
+const header = document.querySelector('.header');
 const allSections = document.querySelectorAll('.section');
-console.log(allSections);
+//console.log(allSections);
+
+document.getElementById('section--1');
+const allButtons = document.getElementsByTagName('button');
+//console.log(allButtons);
+
+//console.log(document.getElementsByClassName('btn'));
+
+// Creating and inserting elements
+// const message = document.createElement('div');
+// message.classList.add('cookie-message');
+// // message.textContent ='We use cookies for improved functionality and analytics.';
+// message.innerHTML =
+//   'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+
+// //header.prepend(message);
+// header.append(message);
+// //header.append(message.cloneNode(true));
+
+// //header.before(message);
+// //header.after(message);
+
+// // Delete elements
+// document
+//   .querySelector('.btn--close-cookie')
+//   .addEventListener('click', function () {
+//     message.remove();
+//   });
+
+//Styles
+// message.style.backgroundColor = '#37383d';
+// message.style.width = '120%';
+// //console.log(message.style.backgroundColor);
+
+// //console.log(getComputedStyle(message).color);
+// //console.log(getComputedStyle(message).height);
+
+// message.style.height =
+//   // Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
+
+//document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+// Attibutes
+const logo = document.querySelector('.nav__logo');
+//console.log(logo.alt);
+//console.log(logo.src);
+
+logo.setAttribute('company', 'Banklist');
+
+const link = document.querySelector('.nav__link--btn');
+//console.log(link.href);
+//console.log(link.getAttribute('href'));
+
+// Data attributes
+//console.log(logo.dataset.versionNumber);
+
+//classes
+logo.classList.add('C', 'm');
+logo.classList.remove('C');
+logo.classList.toggle('C');
+logo.classList.contains('C');
+
+// Don't use like this
+logo.className = 'Kelvin';
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  // console.log(s1coords);
+  // console.log(e.target.getBoundingClientRect());
+  // console.log('Current scroll (X/Y)', window.pageXOffset, window.pageYOffset);
+
+  // Scrolling
+  // window.scrollTo(
+  //   s1coords.left + window.pageXOffset,
+  //   s1coords.top + window.pageYOffset
+  // );
+
+  /* OLD WAY OF DOING THE SMOOTH SCROLLING
+  window.scrollTo({
+    left: s1coords.left + window.pageXOffset,
+    top: s1coords.top + window.pageYOffset,
+    behavior: 'smooth',
+  }); */
+
+  // NEW WAY
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
